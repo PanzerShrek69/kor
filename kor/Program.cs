868ffa874a2681b76e3c7ff9dcaa840ef204753d
Zsombor <zsombor.udvarhelyi.gmail.com>
2022-09-15 10:19:42 +0200

    Add project files.

---
+++ kor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sugar;
+
+            Console.Write("Kérem a kör sugarát: ");
+
+            sugar = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Terulet: {Math.Pow(sugar, 2) * Math.PI}");
+            Console.WriteLine($"Kerulet: {2 * sugar * Math.PI}");
+            
+            Console.ReadKey();
+        }
+    }
+}
